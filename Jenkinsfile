9ef0aa92833fa21df1675540ad66538f9ffea81d
Jeremi Alcala <jeremialcala@gmail.com>
2024-05-23 10:29:07 -0400

    Changing to docker agent on jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,12 +1,9 @@
 pipeline {
-    agent any
+    agent { dockerfile true }
 
     stages {
         stage('Deploy') {
-            steps {
-                sh 'docker build -t api-python-fast-images:latest .'
-                sh 'docker run -it -p 5004:5002 --name api-python-fast-images --restart unless-stopped -d api-python-fast-images'
-            }
+            sh 'echo "Deploying"'
         }
     }
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -rf api-python-fast-images/
 git clone https://github.com/jeremialcala/api-python-fast-images.git
+cp config.env api-python-fast-images/config.env
 cd api-python-fast-images/ || exit
 git checkout develop
 git pull
@@ -8,4 +10,4 @@ docker rm -f api-python-fast-images
 docker rmi api-python-fast-images
 docker system prune -a -f
 docker build -t api-python-fast-images:latest .
-docker run -it -p 5004:5002 --name api-python-fast-images -d api-python-fast-images
+docker run -it -p 5004:5002 --name api-python-fast-images --restart unless-stopped -d api-python-fast-images
